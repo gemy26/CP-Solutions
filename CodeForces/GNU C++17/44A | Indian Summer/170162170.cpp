5e7909ee550bea8836ffb2a25d9cc67c12397586
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:49:42 +0300

    Committing files

---
+++ CodeForces/GNU C++17/44A | Indian Summer/170162170.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include<string.h>
+#define ll long long
+#define el "\n"
+using namespace std;
+//------is prime---------//
+bool isprime(int x)
+{
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0)
+            return false;
+    return true;
+}
+//--------GCD---------------//
+ 
+//-----------------------//
+const int AK=5e5+5;
+const long long VVB=5e9+5;
+//----------------------//
+int main() {
+   int t;
+   cin>>t;
+ 
+  set<pair<string,string>>st;
+    for (int i = 0; i < t; ++i) {
+        pair<string ,string>p;
+        string x,y,s;
+        //cin>>x>>y;
+      cin>> p.first;
+       cin>>p.second;
+       st.insert(p);
+    }
+    cout<<st.size();
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
