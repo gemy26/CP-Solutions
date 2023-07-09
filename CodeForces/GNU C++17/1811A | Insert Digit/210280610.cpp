fb8c0a3fe6fb5461526683f90bd87400d495009c
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:23:44 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1811A | Insert Digit/210280610.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+ 
+bool cmp(const pair<int, int>& a,const pair<int, int>& b){
+    if(a.first == b.first)
+        return a.second > b.second;
+ 
+        return a.first < b.first;
+}
+ 
+ 
+//bool cmp(const string& a,const string& b){return a+b < b+a;}
+ 
+ 
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+ 
+ 
+ 
+ 
+void RunCase() {
+    int n,m;
+    string s,t;
+     bool f= false;
+    cin >> n >> m >> s;
+    for (int i = 0; i < n ; ++i) {
+        if(m > s[i]-'0' && !f){
+            cout<<m;
+            f= true;
+        }
+            cout<<s[i];
+    }
+    if(!f){cout<<m;}
+    cout<<el;
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+    HereWeGo();
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+ 
+    int t = 1;
+    cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+ 
+        RunCase();
+ 
+    }
+    return 0;
+ 
+}
