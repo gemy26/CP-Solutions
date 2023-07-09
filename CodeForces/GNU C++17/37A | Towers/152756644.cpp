15dd65e0b84658c48a63a88113994eff17715378
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:54:53 +0300

    Committing files

---
+++ CodeForces/GNU C++17/37A | Towers/152756644.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isprime(int x)
+{
+  for (int i = 2; i <= x / i; i++)
+    if (x % i == 0)
+      return false;
+  return true;
+}
+int main()
+{
+ int n;
+ int maxi=-9999999;
+ int sum=0;
+ cin>>n;
+ int frq[9999]={0};
+ int a[n];
+ for(int i=0;i<n;i++)
+ {
+  cin>>a[i];
+ }
+ for(int i=0;i<n;i++)
+ {
+  frq[a[i]]++;
+ }
+ for(int i=0;i<=1000;i++)
+ {
+  if(frq[i]){
+    sum++;
+  }
+  if(frq[i]>maxi){
+    maxi=frq[i];
+  }
+ }
+ 
+ cout<<maxi<<" "<<sum;
+ 
+}
