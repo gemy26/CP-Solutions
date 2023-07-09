91be5b96265f48ac66c7c3cefbfde9e59c1f0053
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:55:17 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1608A | Find Array/152583183.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--)
+ {
+  int x;
+cin>>x;
+for(int i=0;i<x;i++)
+{
+  cout<<2+i<<" ";
+ }
+ cout<<endl;
+}
+ 
+}
+ 
+ 
+ 
