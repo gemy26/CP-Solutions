b1f5798404fbcf9978a7218d8c8d5e8426790685
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:58:23 +0300

    Committing files

---
+++ CodeForces/GNU C++17/959A | Mahmoud and Ehab and the even-odd game/147032768.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//#include<bits/stdc++>
+#include<string>
+using namespace std;
+int main( ){
+ 
+   int n;
+   cin>>n;
+   if(n%2==0){
+     cout<<"Mahmoud";
+   }
+   else 
+   cout<<"Ehab";
+ 
+ 
+    return 0;
+}
