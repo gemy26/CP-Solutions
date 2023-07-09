679724de7ddf09c447b4a073ea65004e633d33d2
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:56:53 +0300

    Committing files

---
+++ CodeForces/GNU C++17/486A | Calculating Function/148590684.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string> 
+#include<vector>
+#include<cmath>
+#include<sstream>
+ 
+ 
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+  
+ 
+ 
+   long long n;
+   cin>>n;
+   long long sum=0;
+   long long sum1=0;
+   
+     if(n%2==0){
+       cout<<n/2;
+     }
+     else{cout<<((n/2)-n);}
+   
+   
+ 
+   
+return 0;
+}
