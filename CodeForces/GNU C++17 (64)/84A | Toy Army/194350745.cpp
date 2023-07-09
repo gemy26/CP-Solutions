b1d86c6a46950df152c2f5560a2f30c3c645da96
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:40:59 +0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/84A | Toy Army/194350745.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second < b.second;}
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+int main(){
+   
+HereWeGo();
+ int t;
+    cin>>t;
+ 
+   cout<<t + t/2;
+ 
+ 
+   
+ 
+}
+ 
