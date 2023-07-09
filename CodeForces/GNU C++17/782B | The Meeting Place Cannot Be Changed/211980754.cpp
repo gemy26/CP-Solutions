00df3528b8bf6e599c7d1a2ea6aeef6f434b4934
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:23:22 +0300

    Committing files

---
+++ CodeForces/GNU C++17/782B | The Meeting Place Cannot Be Changed/211980754.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+void setIO(string name = "") {
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+ 
+bool cmp(const pair<int, int>& a,const pair<int, int>& b){
+ 
+ 
+    if (a.second < b.second){
+        return true;
+    }
+    return false;
+}
+ 
+ 
+//bool cmp(const vector<int>&a,const vector<int>&b){return b[1] > a[1];}
+ 
+ 
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+ 
+ 
+int n;
+    vector<double>v;
+    vector<double>b;
+bool good(double mid){
+double mini=1e9;
+double maxi=-1e9;
+    for (int i = 0; i < n ; ++i) {
+        mini = min(mini,v[i]+(b[i]*mid));
+        maxi = max(maxi,v[i]-(b[i]*mid));
+    }
+    return  (mini >= maxi);
+ 
+}
+ 
+ 
+void RunCase() {
+cin >> n;
+v.resize(n);
+b.resize(n);
+ 
+    for (int i = 0; i < n ; ++i) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n ; ++i) {
+        cin >> b[i];
+    }
+double l=0,r=1e9;
+double mid;
+    for (int i = 0; i < 1000 ; ++i) {
+        mid = (l+r)/2;
+        if(good(mid)){
+            r = mid;
+        }else{
+            l = mid;
+        }
+    }
+    cout<<fixed<<setprecision(10)<<l<<el;
+ 
+ 
+}
+ 
+ 
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+*/
+ 
+ 
+ 
+ 
+int main(){
+    HereWeGo();
+//    setIO(NULL);
+ 
+    int t = 1;
+//    cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+ 
+        RunCase();
+ 
+    }
+    return 0;
+ 
+}
