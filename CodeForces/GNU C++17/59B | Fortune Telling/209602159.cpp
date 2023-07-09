f037718a4dbf2b31cee4e63d9edf09418b67f98b
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:24:05 +0300

    Committing files

---
+++ CodeForces/GNU C++17/59B | Fortune Telling/209602159.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+ 
+//bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second > b.second;}
+bool cmp(const string& a,const string& b){return a.size() < b.size();}
+ 
+ 
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+int n,m;
+const int maxn = 1e5;
+vector<vector<pii>>G; // {node,cost}
+vector<int>parent(maxn);
+vector<ll>costs(maxn+1,INF);
+//vector<bool>vis(maxn,false);
+ 
+//void dij(int src){
+//
+//    priority_queue<pii,vector<pii>,greater<pii>>q;
+//
+//    //set<pll>s; // {cost,node}
+//    // s.insert({0,src});
+//    q.push({0,src});
+//    costs[src]=0;
+//    parent[src]=-1;
+//    while (!q.empty()){
+//
+//
+//        ll cost = q.top().first;
+//        int node = q.top().second;
+//        q.pop();
+//
+//        if(cost > costs[node])
+//            continue;
+//
+//        for(auto child : G[node]){
+//            if(costs[child.first] > costs[node]+child.second){
+//                q.push({child.second+ costs[node],child.first});
+//                parent[child.first] = node;
+//                costs[child.first] = costs[node]+child.second;
+//            }
+//        }
+//
+//    }
+//
+//}
+ 
+ 
+ 
+ 
+ 
+void RunCase(){
+   int n;
+   cin >> n;
+   vector<int>v(n);
+    for (int i = 0; i < n ; ++i) {
+        cin >> v[i];
+    }
+   ll sum = 0;
+   int od=0;
+   int f=1;
+   sort(all(v));
+    for (int i = 0; i < n; ++i) {
+        sum += v[i];
+        if((v[i]&1) && f){
+            od = v[i];
+            f=0;
+        }
+    }
+    if(sum&1){
+        return void(cout<<sum);
+    }else{
+        if(od>0){
+            return void(cout<<sum-od);
+        }
+    }
+    return void(cout<<0);
+ 
+}
+ 
+ 
+ 
+int main(){
+    HereWeGo();
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+        RunCase();
+    }
+    return 0;
+ 
+}
