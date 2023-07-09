942742d22f0a9cc9ff3c3ff36b615d310f01ad84
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:56:31 +0300

    Committing files

---
+++ CodeForces/GNU C++17/534A | Exam/148734674.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cin>>n;
+ if(n==1){
+  cout<<"1\n1"; 
+  return 0;
+ }
+ if(n==2){
+cout<<"1\n1"; 
+  return 0;
+ }
+ if(n==3){
+  cout<<"2\n1 3";
+  return 0;
+ }
+ if(n==4){
+  cout<<"4\n3 1 4 2 ";
+  return 0;
+ }
+ else {
+  cout<<n<<endl;
+  for(int i=1;i<=n;i++){
+   if(i%2!=0){
+    cout<<i<<" ";
+   }
+  }
+   for(int i=2;i<=n;i++){
+    if(i%2==0){
+     cout<<i<<" ";
+    }
+   }
+  }
+ return 0;
+}
