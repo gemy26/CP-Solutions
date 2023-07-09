e9a6582a9f87f172905157c78e6d35810c252945
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:50:49 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1277A | Happy Birthday, Polycarp!/165059552.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include<vector>
+#define ll long long
+#define el "\n"
+using namespace std;
+//------is prime---------//
+bool isprime(int x)
+{
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0)
+            return false;
+    return true;
+}
+//----------binary search--------//
+int binarysearch(int a[],int key,int low,int high)
+{
+    int middle=(low+high)/2;
+    if(a[middle]==key) return middle;
+    if(key>middle)return binarysearch(a,key,middle+1,high);
+    else
+        return binarysearch(a,key,low ,middle-1);
+}
+//--------GCD---------------//
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ 
+//-----------------------//
+const int AK=5e5+5;
+const long long VVB=5e9+5;
+//----------------------//
+int main()
+{
+ 
+    ll numbers[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 99999, 111111, 222222, 333333, 444444, 555555, 666666, 777777, 888888, 999999, 1111111, 2222222, 3333333, 4444444, 5555555, 6666666, 7777777, 8888888, 9999999, 11111111, 22222222, 33333333, 44444444, 55555555, 66666666, 77777777, 88888888, 99999999, 111111111, 222222222, 333333333, 444444444, 555555555, 666666666, 777777777, 888888888, 999999999, 9999999999};
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+ 
+        for(int i=0;i<=81;i++)
+        {
+            if(n<numbers[i])
+            {
+                cout<<i<<endl;
+                break;
+            }
+        }
+    }
+ 
+}
+// 11-100==>9
+// 101-1000==>9
+ 
+ 
