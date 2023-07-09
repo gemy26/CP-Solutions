cf01f67f9b846bd1f7a531eac053207cb440e5aa
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:24:12 +0300

    Committing files

---
+++ CodeForces/GNU C++17/514B | Han Solo and Lazer Gun/209325737.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+ 
+bool cmp(const pair<int, double>& a,const pair<int, double>& b){
+    if(a.second == b.second)
+        return a.first < b.first;
+    return a.second > b.second;
+}
+//bool cmp(const string& a,const string& b){return a.size() < b.size();}
+ 
+ 
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+ 
+ 
+ 
+void RunCase() {
+ 
+    int n;
+    cin >> n;
+    set<double>s;
+    int x,y;
+    cin >> x >> y;
+    set<int>z;
+    int k = 0;
+    while(n--){
+        int a,b;
+        cin >> a >> b;
+        if(x-a == 0){
+          k=1;
+        }else{
+            double q = (y-b);
+            q = q/(x-a);
+            s.insert(q);
+        }
+    }
+    cout<<s.size()+k<<el;
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main(){
+    HereWeGo();
+// freopen("maxcross.in", "r", stdin);
+// freopen("maxcross.out", "w", stdout);
+ 
+    int t = 1;
+    //cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+ 
+        RunCase();
+ 
+    }
+    return 0;
+ 
+}
