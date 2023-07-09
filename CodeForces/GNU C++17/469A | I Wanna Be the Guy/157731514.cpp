93b893950c9bdf665f0423c91ea1c07be134f3fe
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:52:24 +0300

    Committing files

---
+++ CodeForces/GNU C++17/469A | I Wanna Be the Guy/157731514.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include<vector>
+ 
+#define ll long long
+#define el "\n"
+using namespace std;
+//------is prime---------//
+bool isprime(int x)
+{
+  for (int i = 2; i <= x / i; i++)
+    if (x % i == 0)
+      return false;
+  return true;
+}
+//----------binary search--------//
+ int binarysearch(int a[],int key,int low,int high)
+ {
+  int middle=(low+high)/2;
+  if(a[middle]==key) return middle;
+  if(key>middle)return binarysearch(a,key,middle+1,high);
+  else
+    return binarysearch(a,key,low ,middle-1);
+ }
+ //--------GCD---------------//
+ int gcd(int a, int b)
+{
+      
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+    if (a == b)
+        return a;
+    if (a > b)
+    return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+//-----------------------//
+const int AK=5e5+5;
+const long long VVB=5e9+5; 
+//----------------------//
+int main()
+{
+    
+int n;
+cin>>n;
+set<int>a;
+int x; cin>>x;
+for(int i=0;i<x;i++){
+  int z; cin>>z;
+  a.insert(z);
+}
+int y; cin>>y;
+for(int i=0;i<y;i++)
+{
+  int z; cin>>z;
+  a.insert(z);
+}
+if(a.size()==n)
+{
+  cout<<"I become the guy.";
+  return 0;
+}
+else
+{
+  cout<<"Oh, my keyboard!";
+  return 0;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
