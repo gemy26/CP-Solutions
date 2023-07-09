c9be5d627af6b7753bff25bf28f332193d10b68b
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:46:06 +0300

    Committing files

---
+++ CodeForces/GNU C++17/699A | Launch of Collider/181654121.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define el "\n"
+#include <fstream>
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+using namespace std;
+//------is prime---------//
+int isPrime(int k)
+{
+ 
+    if (k <= 1)
+        return 0;
+    if (k == 2 || k == 3)
+        return 1;
+    if (k % 2 == 0 || k % 3 == 0)
+        return 0;
+ 
+    for (int i = 5; i * i <= k; i = i + 6)
+        if (k % i == 0 || k % (i + 2) == 0)
+            return 0;
+ 
+    return 1;
+}
+ 
+bool palindrome(string s)
+{
+    string p = s;
+    reverse(p.begin(), p.end());
+    if (s == p)
+        return true;
+    else
+        return false;
+}
+ 
+//-----------------------//
+const int AK = 5e5 + 5;
+const long long VVB = 5e9 + 5;
+//----------------------//
+bool sortbysecdesc(const pair<int, int> &a, const pair<int, int> &b)
+{
+    return a.second > b.second;
+}
+int mod = 1e9 + 7;
+int main()
+{
+    fast_cin();
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int a[n];
+    vector<int>v;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    
+    for (size_t i = 0; i < s.size()-1; i++)
+    {
+        if(s[i]=='R' &&s[i+1]=='L'){
+            int cnt = abs(a[i] - ((a[i] + a[i+1]) / 2));
+            if (cnt)
+                v.push_back(cnt);
+        }
+    }
+    if(v.size()){
+        cout<<*min_element(v.begin(),v.end());
+        return 0;
+    }
+    cout<<-1;
+}
