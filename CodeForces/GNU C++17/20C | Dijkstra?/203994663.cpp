12949ed11cbb90da03ff69a16dc5f7a632c6a901
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:25:27 +0300

    Committing files

---
+++ CodeForces/GNU C++17/20C | Dijkstra?/203994663.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+ 
+//bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second > b.second;}
+bool cmp(const string& a,const string& b){return a.size() < b.size();}
+ 
+ 
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+int n,m;
+const int maxn = 1e5;
+vector<vector<pii>>G; // {node,cost}
+vector<int>parent(maxn);
+vector<ll>costs(maxn+1,INF);
+//vector<bool>vis(maxn,false);
+ 
+void dij(int src){
+ 
+    priority_queue<pii,vector<pii>,greater<pii>>q;
+ 
+    //set<pll>s; // {cost,node}
+    // s.insert({0,src});
+    q.push({0,src});
+    costs[src]=0;
+    parent[src]=-1;
+    while (!q.empty()){
+ 
+ 
+        ll cost = q.top().first;
+        int node = q.top().second;
+        q.pop();
+ 
+        if(cost > costs[node])
+            continue;
+ 
+        for(auto child : G[node]){
+            if(costs[child.first] > costs[node]+child.second){
+                q.push({child.second+ costs[node],child.first});
+                parent[child.first] = node;
+                costs[child.first] = costs[node]+child.second;
+            }
+        }
+ 
+    }
+ 
+}
+ 
+ 
+ 
+ 
+ 
+void RunCase(){
+    cin >> n >> m;
+    G.resize(n+1);
+    for (int i = 0; i < m ; ++i) {
+        int u,v,w;
+        cin >> u >> v >> w;
+        G[u].push_back({v,w});
+        G[v].push_back({u,w});
+    }
+ 
+    dij(1);
+    if( costs[n]==INF)
+        return void(cout<<-1<<el);
+    else{
+        vector<int>path;
+        //path.pb(1);
+        int node = n;
+ 
+        while (node != -1){path.pb(node);node=parent[node]; }
+        //
+       // path.pb(1);
+        reverse(all(path));
+ 
+        for(auto i : path)
+            cout<<i<<" ";
+    }
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+int main(){
+    HereWeGo();
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+        RunCase();
+    }
+    return 0;
+ 
+}
