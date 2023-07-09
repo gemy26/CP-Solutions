69baed21408b880c7489745ee3a0158d04128cab
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:41:05 +0300

    Committing files

---
+++ CodeForces/GNU C++17 (64)/977C | Less or Equal/194233941.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second < b.second;}
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+ 
+int main(){
+    HereWeGo();
+ 
+ 
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for (int i = 0; i < n ; ++i) {
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end());
+    if(n==1&&k==0&&v[0]>1){cout<<1; return 0;}
+    if(n==1&&k){cout<<v[0]; return 0;}
+    if(k==0){
+        if(v[0]>1)
+        cout<<v[0]-1;
+        else
+            cout<<-1;
+        return 0;
+    }
+ 
+ 
+    int x = *max_element(v.begin(),v.begin()+k);
+    for (int i = k; i < n ; ++i) {
+        if(v[i] <= x) {
+            cout << -1;
+            return 0;
+        }
+    }
+    cout<<x;
+ 
+ 
+}
