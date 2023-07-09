03b16ed9abdfab16f455393f6166cb81e57140cf
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:56:38 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1626A | Equidistant Letters/148652299.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string> 
+#include<vector>
+#include<cmath>
+#include<sstream>
+using namespace std;
+int main()
+{
+ string s;
+ 
+ int t;
+ cin>>t;
+ while(t--){
+     cin>>s;
+sort(s.begin(),s.end());
+ cout<<s<<endl;
+ }
+ 
+ 
+ 
+return 0;
+}
