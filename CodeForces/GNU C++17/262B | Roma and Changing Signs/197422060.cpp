66ad7506105215a7fd473ed1fc08638b92b0735f
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:39:56 +0300

    Committing files

---
+++ CodeForces/GNU C++17/262B | Roma and Changing Signs/197422060.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second < b.second;}
+// here we go
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+ 
+int main(){
+    HereWeGo();
+   int n,k;
+   cin >> n >>k;
+   vector<int>v(n);
+    for (int i = 0; i < n ; ++i) {
+        cin >> v[i];
+    }
+ 
+    for (int i = 0; i < n ; ++i) {
+        if(!k)
+            break;
+        if(v[i]<0 && k ) { v[i] = abs(v[i]); k--;}
+ 
+    }
+    ll sum =0;
+    sort(all(v));
+    if(k){
+        if(k%2!=0 && v[0]>=0){
+            v[0]*=-1;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        sum += v[i];
+    }
+cout<<sum;
+ 
+}
