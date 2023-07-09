c664e8ad95d40dbc3f179462556801ee2d47ccf7
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:51:11 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1703B | ICPC Balloons/163835066.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include<vector>
+#define ll long long
+#define el "\n"
+using namespace std;
+//------is prime---------//
+bool isprime(int x)
+{
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0)
+            return false;
+    return true;
+}
+ 
+//----------binary search--------//
+int binarysearch(int a[],int key,int low,int high)
+{
+    int middle=(low+high)/2;
+    if(a[middle]==key) return middle;
+    if(key>middle)return binarysearch(a,key,middle+1,high);
+    else
+        return binarysearch(a,key,low ,middle-1);
+}
+//--------GCD---------------//
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ 
+//-----------------------//
+const int AK=5e5+5;
+const long long VVB=5e9+5;
+//----------------------//
+int main() {
+int n; cin>>n;
+ set<char>st;
+ 
+    for (int i = 0; i < n ; ++i) {
+        int cnt=0;
+        int t; cin>>t;
+        for (int j = 0; j < t; ++j) {
+            char ch; cin>>ch;
+            if(st.count(ch)) cnt++;
+            else {
+                cnt += 2;
+                st.insert(ch);
+            }
+        }
+        cout<<cnt<<el;
+        st.clear();
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
