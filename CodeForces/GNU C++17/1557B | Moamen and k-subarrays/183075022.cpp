906d2885451061c2ebde2be494bbfec7a44c51f7
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:45:06 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1557B | Moamen and k-subarrays/183075022.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define el "\n"
+#define all(x) x.begin(), x.end()
+#include <fstream>
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+using namespace std;
+//------is prime---------//
+int isPrime(int k)
+{
+ 
+    if (k <= 1)
+        return 0;
+    if (k == 2 || k == 3)
+        return 1;
+    if (k % 2 == 0 || k % 3 == 0)
+        return 0;
+ 
+    for (int i = 5; i * i <= k; i = i + 6)
+        if (k % i == 0 || k % (i + 2) == 0)
+            return 0;
+ 
+    return 1;
+}
+ 
+bool palindrome(string s)
+{
+    string p = s;
+    reverse(p.begin(), p.end());
+    if (s == p)
+        return true;
+    else
+        return false;
+}
+ 
+//-----------------------//
+const int AK = 5e5 + 5;
+const long long VVB = 5e9 + 5;
+//----------------------//
+bool sortbysecdesc(const pair<int, int> &a, const pair<int, int> &b)
+{
+    return a.second > b.second;
+}
+int mod = 1e9 + 7;
+int main()
+{
+    fast_cin();
+   int t;
+   cin>>t;
+   while(t--){
+    int k,n;
+     cin>>n>>k;
+     vector<pair<int,int>>v(n);
+     for (int i = 0; i < n; ++i)
+     {
+         cin>>v[i].first;v[i].second=i;
+     }
+     int cnt=0;
+     sort(all(v));
+     for (int i = 1; i < n; ++i)
+     {
+         if(v[i-1].first < v[i].first && v[i].second - v[i-1].second == 1)
+            continue;
+        else
+            cnt++;
+     }
+     if(cnt >= k)
+        cout<<"NO"<<el;
+    else
+        cout<<"YES"<<el;
+ 
+   }
+}
