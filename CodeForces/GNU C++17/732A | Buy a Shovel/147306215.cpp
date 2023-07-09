405a3340ba7b7aab1eb64e15a7118beef19bdc5f
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:58:10 +0300

    Committing files

---
+++ CodeForces/GNU C++17/732A | Buy a Shovel/147306215.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+ int x,y,counter=0;
+ int flag=0;
+ cin>>y>>x;
+ int i=1;
+ while(true){
+    
+    
+   
+ 
+      if(((y*i)%10==x)||((y*i)%10==0)){
+           cout<<i;
+          return 0;
+     }
+ 
+++i;
+ 
+ }
+ 
+  
+ 
+ 
+ 
+ 
+   
+}
