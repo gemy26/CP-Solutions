9b30c15f2b96fe191c8e32820a0660443255eb40
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:57:38 +0300

    Committing files

---
+++ CodeForces/GNU C++17/630A | Again Twenty Five!/147796342.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int temp=1;
+    if(n>=2&&n<=2000000000000000000){
+ 
+    cout<<25;}
+}
+            
