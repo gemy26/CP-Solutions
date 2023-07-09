b810b92efc157888238f254159172940aa056e26
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:51:47 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1692A | Marathon/161514666.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+#define  ll long long
+//#define r return 0
+#define yes cout<<"YES";
+#define no cout<<"NO";
+#define el cout<<"\n";
+const int AK=5e5+5;
+const long long VVB=5e9+5; 
+using namespace std;
+bool isprime(int x)
+{
+  for (int i = 2; i <= x / i; i++)
+    if (x % i == 0)
+      return false;
+  return true;
+}
+int gcd(int a, int b)
+{
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+    if (a == b)
+        return a;
+    if (a > b)
+    return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ 
+int main()
+{
+  int t; cin>>t;
+  while(t--)
+  {
+  int a[4];
+  for(int i=0;i<4;i++)
+cin>>a[i];
+int x=a[0];
+int cnt=0;
+for(int i=1;i<4;i++)
+{
+  if(a[i]>x)
+    cnt++;
+}
+cout<<cnt<<endl;
+}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
+ 
