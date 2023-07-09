ca26fae2112831570f4dc2599359ec4f5066a9e1
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:57:44 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1371A | Magical Sticks/147659300.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+int main(){
+  int  t;
+  cin>>t;
+  while(t--){
+      int n;
+      cin>>n;
+      if(n%2==0){
+          cout<<n/2<<endl;
+      }
+      else if(n%2!=0){
+          cout<<(n+1)/2<<endl;
+      }
+  }
+ 
+  
+ 
+ 
+    return 0;
+}
