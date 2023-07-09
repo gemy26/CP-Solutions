735fc3899a234056a7e35f28cc7c00cccaa4d785
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-07-09 21:22:59 +0300

    Committing files

---
+++ CodeForces/GNU C++17/1846D | Rudolph and Christmas Tree/212712124.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+ll mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+//bool cmp(pair<ll, ll>& a,pair<ll, ll>& b){return a.second > b.second;}
+bool cmp(const string& a,const string& b){return a.size() < b.size();}
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+ 
+ 
+ 
+void RunCase(){
+    int n,d,h;
+    cin >> n >> d >> h;
+    vector<int>v(n);
+    double res=0;
+    for (int i = 0; i < n; ++i)
+    {
+ 
+        cin >> v[i];
+    }
+    sort(all(v));
+    for (int i = 1; i < n ; ++i)
+    {
+ 
+        int dif = v[i]-v[i-1];
+        double mini = 0;
+        if(dif < h){
+ 
+            double hi =(h*1.0)-(dif*1.0);
+            double base = (hi/h)*d;
+            mini = (base*1.0*hi)/2.0;
+//            cout<<mini<<"  ";
+        }
+        res += ((d*1.0)*(h*1.0))/(2.0*1.0);
+        res -= mini;
+ 
+ 
+    }
+    res += ((d*1.0)*(h*1.0))/(2.0*1.0);
+    cout<<setprecision(15)<<res<<el;
+//    cout<<el<<el;
+//    cout<<setpresision(13)<<res<<el;
+ 
+}
+ 
+int main(){
+    HereWeGo();
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+    int t = 1;
+     cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        //cout<<"Case #:"<<i+1<<el;
+        RunCase();
+    }
+ 
+}
