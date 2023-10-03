52e475ecd2565e375398e90293089485ddc0dfee
gemy26 <102263771+gemy26@users.noreply.github.com>
2023-10-03 09:32:41 +0300

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc293_b/45754258.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+
+
+
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define  el '\n'
+#define all(x) x.begin(),x.end()
+
+
+const ll  mod = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int N=2e5+5;
+
+void setIO(string name = "") {
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+bool cmp(const pair<int, int>& a,const pair<int, int>& b){
+    if (a.first  == b.first){return a.second < b.second;}
+    return a.first > b.first;
+}
+
+
+//bool valid(int i,int j){
+//    return i >= 0 && i < n && j >= 0 && j < m;
+//}
+
+
+
+int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+const char dc[4]={'U','R','D','L'};
+#define HereWeGo() ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr);
+
+
+
+
+
+void RunCase() {
+
+int n;
+cin >> n;
+set<int>st;
+vector<int>v(n+1);
+for(int i=1;i<=n;i++)cin >> v[i];
+for(int i=1;i<=n ; i++){
+     if(st.count(i) == 0){
+        st.insert(v[i]);
+    }
+}
+vector<int>A;
+for(int i=1;i<=n;i++)
+    if(st.count(i) == 0){A.pb(i);}
+    cout<<A.size()<<el;
+    for(auto i : A)cout<<i<<" ";
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    HereWeGo();
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+    int t = 1;
+//    cin >> t;
+    for (int i = 0; i < t ; ++i) {
+        RunCase();
+
+    }
+
+
+
+
+
+
+
+}
